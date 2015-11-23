e1d4d710f3e7419c6f7937d8887ddae46b9742cc
andreibotnari <c13383341@mydit.ie>
2015-11-23 16:44:55 +0000

    commit

--- list-aws-queues.py
+++ list-aws-queues.py
@@ -10,11 +10,21 @@ from boto.sqs.connection import SQSConnection
 from boto.exception import SQSError
 import sys
 
-# Get the keys from a specific url and then use them to connect to AWS Service 
-access_key_id = ""
-secret_access_key = ""
+# Get the keys from a specific url and then use them to connect to AWS Service
+print ("Boto Vrsion"+boto.Version)
 
-# Set up a connection to the AWS service. 
+req = urllib2.urlopen('http://ec2-52-30-7-5.eu-west-1.compute.amazonaws.com:81/key')
+req = req.read()
+key = req.split(":")
+
+access_key_id = "[0]"
+secret_access_key = "[1]"
+
+print("ID:" +key[0])
+print("\n" +key[1])
+
+
+# Set up a connection to the AWS service.
 conn = boto.sqs.connect_to_region("eu-west-1", aws_access_key_id=access_key_id, aws_secret_access_key=secret_access_key)
 
 # Get a list of the queues that exists and then print the list out

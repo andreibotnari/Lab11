8bafa7012fc5899e2e7837e59cc2e09bfef05b26
andreibotnari <c13383341@mydit.ie>
2015-11-23 16:40:57 +0000

    commit 2

--- verify-boto-and-keys.py
+++ verify-boto-and-keys.py
@@ -1,8 +1,11 @@
 import urllib2
+import string
 import boto
+print ("Boto Vrsion"+boto.Version)
 
-req= urllib2.request('http://ec2-52-30-7-5.eu-west-1.compute.amazonaws.com:81/key')
-f = urllib2.urlopen(req)
-#
-code = f.read()
-print(code[0:20]+ "\n" + code[21:1]+"\n"+ boto.Version)
+req = urllib2.urlopen('http://ec2-52-30-7-5.eu-west-1.compute.amazonaws.com:81/key')
+req = req.read()
+key = req.split(":")
+
+print("ID:" +key[0])
+print("\n" +key[1])
